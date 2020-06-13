d6470272f908efcd3019e4f3eb34c706c59b250c
Valeryi Savich <relrin78@gmail.com>
2020-06-13 18:51:29 +0300

    Implemented converting regex matches into commands

--- src/parser.rs
+++ src/parser.rs
@@ -4,15 +4,15 @@ use regex::{Match, Regex};
 lazy_static! {
     static ref COMMAND_REGEX: Regex =
         Regex::new(r"\{\{\s*(?P<command>[^\s\(\)]+)\s*\((?P<args>.+)\)\s*\}\}").unwrap();
-    static ref ARG_REGEX: Regex = Regex::new("\'(.*?)\'|\"(.*?)\"").unwrap();
+    static ref ARG_REGEX: Regex = Regex::new(r"\u0027(.*?)\u0027|\u0022(.*?)\u0022").unwrap();
 }
 
 #[derive(Debug)]
 pub struct VaultCommandParser;
 
 #[derive(Debug)]
-pub struct ParsedCommand<'a> {
-    regex_match: Match<'a>,
+pub struct ParsedCommand {
+    regex_match: String,
     command: VaultCommand,
 }
 
@@ -26,20 +26,33 @@ impl VaultCommandParser {
         VaultCommandParser {}
     }
 
-    pub(crate) fn parse(&self, data: &String) -> Vec<ParsedCommand> {
-        Vec::new()
+    pub(crate) fn parse(&self, data: &str) -> Vec<ParsedCommand> {
+        let mut parsed_commands = Vec::new();
+
+        for group in COMMAND_REGEX.captures_iter(data) {
+            let raw_match = group.get(0).map_or("", |v| v.as_str()).to_string();
+            let command_name = group.name("command").map_or("", |v| v.as_str());
+            let args = group.name("args").map_or("", |v| v.as_str());
+
+            // TODO: Add error handling
+            let vault_command = VaultCommand::parse(command_name, args);
+            let command = ParsedCommand::new(raw_match, vault_command);
+            parsed_commands.push(command);
+        }
+
+        parsed_commands
     }
 }
 
-impl<'a> ParsedCommand<'a> {
-    pub(crate) fn new(regex_match: Match<'a>, command: VaultCommand) -> Self {
+impl ParsedCommand {
+    pub(crate) fn new(regex_match: String, command: VaultCommand) -> Self {
         ParsedCommand {
             regex_match,
             command,
         }
     }
 
-    pub(crate) fn regex_match(&self) -> &Match<'a> {
+    pub(crate) fn regex_match(&self) -> &String {
         &self.regex_match
     }
 
@@ -48,13 +61,54 @@ impl<'a> ParsedCommand<'a> {
     }
 }
 
+impl VaultCommand {
+    // TODO: Add error handling
+    // TODO: Add tests for this method
+    pub(crate) fn parse(command_name: &str, args: &str) -> Self {
+        let mut extracted_args: Vec<String> = Vec::new();
+        for capture in ARG_REGEX.captures_iter(args) {
+            let value = capture.get(0).map_or("", |v| v.as_str()).to_string();
+            extracted_args.push(value)
+        }
+
+        match command_name {
+            _ => {
+                let path = extracted_args.get(0).unwrap().clone();
+                let key = extracted_args.get(1).unwrap().clone();
+                VaultCommand::Lookup { path, key }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_parser_returns_lookup_command() {}
+    use crate::parser::{ParsedCommand, VaultCommandParser};
 
     #[test]
-    fn test_parser_returns_multiple_lookup_commands() {}
+    fn test_parser_returns_lookup_command() {
+        let data = "KEY_1: {{ lookup ('/data/storage/dev/', 'aws_key') }}";
+
+        let instance = VaultCommandParser::new();
+        let commands = instance.parse(data);
+
+        assert_eq!(commands.len(), 1);
+    }
+
+    #[test]
+    fn test_parser_returns_multiple_lookup_commands() {
+        // Use these examples
+        // KEY_1: {{ lookup ('/data/storage/dev/', 'key') }}
+        // KEY_2: {{ lookup ('/data/storage/dev/', 'pass') }}
+        // KEY_3: {{ lookup (NOT A KEY) }}
+        // KEY_4: {{ test a commnad }}
+        // KEY_5: {{lookup("/data/storage/dev/",'random_stuff')}}
+        // KEY_6: {{lookup('/data/storage/dev/',"random_stuff2")}}
+        // KEY_7: {{lookup("/data/storage/dev/",'try parse\' this')}}
+        // KEY_8: {{ lookup ("SOME_KEY") }}
+        // KEY_9: {{ lookup ("first", "second", "third") }}
+        // KEY_10: {{ lookup ("escape\" this",) }}
+    }
 
     #[test]
     fn test_parser_returns_no_commands_for_empty_input() {}
